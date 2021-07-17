a95adb95a150b88e2011fcae7fac89ab303b8cf3
Victor Campelo <victor_campelo@outlook.com>
2021-07-17 19:09:47 -0300

    feat (server): added send and receive image file from client

--- server/app.py
+++ server/app.py
@@ -1,17 +1,27 @@
-import os
-from flask import Flask, request, session
-from werkzeug.utils import secure_filename
-from flask_cors import CORS
 import logging
+import os
+import time
+
+from flask import Flask, abort, request, send_from_directory, session, render_template, make_response, jsonify
+from flask_cors import CORS
+from werkzeug.utils import secure_filename
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('HELLO WORLD')
 
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_EXTENSIONS = set(['.png', '.jpg', '.jpeg'])
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = '/home/daslef/Codebase/fileupload-react-flask/server/uploads'
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['UPLOAD_EXTENSIONS'] = ALLOWED_EXTENSIONS
+
 CORS(app, expose_headers='Authorization')
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("index.html")
 
 
 @app.route('/upload', methods=['POST'])
@@ -22,11 +32,24 @@ def fileUpload():
     logger.info("welcome to upload`")
     file = request.files['file']
     filename = secure_filename(file.filename)
+    if filename != '':
+        file_ext = os.path.splitext(filename)[1]
+        print(file_ext)
+        if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+            abort(400)
     destination = "/".join([target, filename])
     file.save(destination)
     session['uploadFilePath'] = destination
-    response = "Whatever you wish too return"
-    return response
+    full_filename = "http://localhost:8000/uploads/test/" + filename
+    print(full_filename)
+    time.sleep(10)
+    return jsonify({"image": full_filename})
+
+
+@app.route('/uploads/test/<filename>')
+def send_file(filename):
+    target = os.path.join(app.config['UPLOAD_FOLDER'], 'test')
+    return send_from_directory(target, filename)
 
 
 if __name__ == "__main__":
